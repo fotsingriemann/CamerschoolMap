0ef47b27cb7b35e196f321472dafb263fb15c530
fotsingriemann <fotsingriemann@gmail.com>
2025-04-12 18:59:54 +0100

    vis

--- CamerschoolMap/build.sh
+++ CamerschoolMap/build.sh
@@ -10,5 +10,11 @@ pip install -r requirements.txt
 # Appliquer les migrations
 python manage.py migrate
 
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.filter(username='admin').exists() or \
+User.objects.create_superuser('admin', 'lucas@gmail.com', 'Lucas1lucas')" \
+| python manage.py shell
+
 # Collecter les fichiers statiques (dans /staticfiles pour Render)
 python manage.py collectstatic --noinput
