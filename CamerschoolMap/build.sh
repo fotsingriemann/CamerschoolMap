5d6410a9a69c6d4f07e35b5c2872cb42c8a4949f
fotsingriemann <fotsingriemann@gmail.com>
2025-04-13 14:05:51 +0100

    taken nicely

--- CamerschoolMap/build.sh
+++ CamerschoolMap/build.sh
@@ -10,11 +10,5 @@ pip install -r requirements.txt
 # Appliquer les migrations
 python manage.py migrate
 
-# Créer le superuser (non interactif)
-DJANGO_SUPERUSER_USERNAME=camerschool \
-DJANGO_SUPERUSER_EMAIL=camerschoolmap@gmail.com \
-DJANGO_SUPERUSER_PASSWORD=camerschool \
-python manage.py createsuperuser --noinput || echo "Superuser déjà existant"
-
 # Collecter les fichiers statiques
 python manage.py collectstatic --noinput
